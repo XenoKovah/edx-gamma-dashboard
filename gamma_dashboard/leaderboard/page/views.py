41a458c79c7b56ad52bfbbfd3fe8bfcc0d038103
Kyrylo Omelchenko <kirill.omelchenko@raccoongang.com>
2020-04-10 13:09:30 +0300

    Integrate gamification leaderboard page into openEdx
    - grace the RG_GAMIFICATION.ENABLED settings flag to inject endpoint urls
    - update the view context to match the settings of lms

--- gamma_dashboard/leaderboard/page/views.py
+++ gamma_dashboard/leaderboard/page/views.py
@@ -4,6 +4,7 @@ Gamma leaderboard views.
 from django.views import View
 
 from edxmako.shortcuts import render_to_response
+from openedx.core.djangoapps.programs.models import ProgramsApiConfig
 
 
 class LeaderboardView(View):
@@ -12,4 +13,10 @@ class LeaderboardView(View):
     """
 
     def get(self, request):
-        return render_to_response('leaderboard/base.html')
+        context = {
+            'user': request.user,
+            'show_dashboard_tabs': True,
+            'show_program_listing': ProgramsApiConfig.is_enabled(),
+        }
+
+        return render_to_response('leaderboard/leaderboard_page.html', context)
--- gamma_dashboard/urls.py
+++ gamma_dashboard/urls.py
@@ -3,7 +3,12 @@ Gamification dashboard urls.
 """
 from django.conf.urls import include, url
 
-urlpatterns = [
-    url(r'api/', include('gamma_dashboard.leaderboard.api.urls', namespace='gamma-api')),
-    url(r'leaderboard/',  include('gamma_dashboard.leaderboard.urls', namespace='gamma-leaderboard'))
-]
+from gamma_dashboard.leaderboard.core.gamma.settings import gamma_settings
+
+urlpatterns = []
+
+if gamma_settings.bridge.get('ENABLED'):
+    urlpatterns.extend([
+        url(r'api/', include('gamma_dashboard.leaderboard.api.urls', namespace='gamma-api')),
+        url(r'leaderboard/',  include('gamma_dashboard.leaderboard.urls', namespace='gamma-leaderboard'))
+    ])
