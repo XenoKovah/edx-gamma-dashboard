67884b7874c5f272e485663897d302052e93949e
Kyrylo Kireiev <kyrylo.kireiev@raccoongang.com>
2025-01-21 17:22:53 +0000

    fix: [NAU-184] Fix Course Leaderboard tab 500

--- course_leaderboard/settings.py
+++ course_leaderboard/settings.py
@@ -2,9 +2,11 @@
 Settings for course leaderboard App.
 """
 
+from path import Path
+
 
 def plugin_settings(settings):
     """
     Read / Update necessary project settings.
     """
-    pass
+    settings.MAKO_TEMPLATE_DIRS_BASE.append(Path(__file__).parent / 'templates')
