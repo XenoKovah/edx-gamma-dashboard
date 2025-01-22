3c6d02590b675f89e8e78bd4a3de1d077bcce68c
KyryloKireiev <kirillkireev888@gmail.com>
2025-01-22 17:48:45 +0200

    fix: [NAU-184] Fix Course Leaderboard tab 500 v2

--- course_leaderboard/settings.py
+++ course_leaderboard/settings.py
@@ -2,11 +2,28 @@
 Settings for course leaderboard App.
 """
 
-from path import Path
+
+from os.path import (
+    abspath,
+    dirname,
+    join,
+)
+
+
+PROJECT_BASE_DIR = dirname(abspath(__file__))
+
+
+def _absolute_path_to(path):
+    """
+    Return an absolute path of a project's sub-path `path`.
+    """
+    return join(PROJECT_BASE_DIR, path)
 
 
 def plugin_settings(settings):
     """
     Read / Update necessary project settings.
     """
-    settings.MAKO_TEMPLATE_DIRS_BASE.append(Path(__file__).parent / 'templates')
+    settings.MAKO_TEMPLATE_DIRS_BASE.append(
+        _absolute_path_to('templates')
+    )
